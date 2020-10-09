30ea55c9c3183c22c6f64b6654adde367187e931
alexis echard <echarda@gmail.com>
2020-10-09 13:14:12 +0200

    Addition of null check in ControlValves.cs

--- actuatorSimulation/Classes/ControlValve.cs
+++ actuatorSimulation/Classes/ControlValve.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -53,10 +53,6 @@ namespace actuatorSimulation
                 
             }
         }
-
-
-
-
 
         /// <summary>
         /// Constructor with parameters
